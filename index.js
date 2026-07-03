30bb14d7d86fa1afd0bd55d236f2e2decca3a1db
abhishekdav003 <abhishekdav003@gmail.com>
2026-07-03 09:58:45 +0530

    Tables Created in Mysql

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express()
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database:'testdb'
-})
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "busbooking",
+});
 
 connection.connect((err) => {
   if (err) {
@@ -16,11 +16,75 @@ connection.connect((err) => {
   }
   console.log("Database connected")
 
-  const creationQuery = `create table School(
+  const creationQuery = `create table if not exists School(
   StudentID INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(20),
   email VARCHAR(20)
+  )`;
+
+  const userQuery = `create table if not exists Users(
+  id int auto_increment primary key,
+  name varchar(255),
+  email varchar(255)
   )`
+
+  const busQuery = `create table if not exists Buses(
+  id int auto_increment primary key,
+  busNumber varchar(50),
+  totalSeats int,
+  availableSeats int
+  )`
+
+  const bookingQuery = `create table if not exists Booking(
+  id int auto_increment primary key,
+  seatNumber int
+  )`
+
+  const paymentQuery = `create table if not exists Payments(
+  id int auto_increment primary key,
+  amountPaid decimal(10,2),
+  paymentStatus varchar(50)
+  )`
+
+  connection.execute(paymentQuery, (err) => {
+    if (err) {
+      console.log(err)
+      connection.end()
+      return
+    }
+    console.log(
+      "payment table created"
+    )
+  })
+
+  connection.execute(bookingQuery, (err) => {
+    if (err) {
+      console.log(err)
+      connection.end()
+      return
+    }
+    console.log("Booking table created")
+  })
+
+  connection.execute(busQuery, (err) => {
+    if (err) {
+      console.log(err)
+      connection.end()
+      return
+    }
+    console.log("Bus Table created")
+  })
+
+  connection.execute(userQuery, (err) => {
+    if (err) {
+      console.log(err)
+      connection.end()
+      return
+    }
+    console.log("User Table created")
+  })
+
+  
 
   connection.execute(creationQuery, (err) => {
     if (err) {
