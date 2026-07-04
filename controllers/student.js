1c56f33107c1c3a02f63cbc9f71355126131844c
abhishekdav003 <abhishekdav003@gmail.com>
2026-07-04 15:26:41 +0530

    CRUD Operations using sequelize

--- controllers/student.js
+++ controllers/student.js
@@ -1,64 +1,176 @@
-const db = require("../utils/db")
+const Student = require("../models/students");
 
-const studentCheck = (req, res) => {
-  res.send("Student is access")
-}
+// Insert
+const addStudent = async (req, res) => {
+  try {
+    const { name, email } = req.body;
 
-const addStudent = (req, res) => {
-  const { name, email } = req.body
-  const insertQuery = `insert into students (name, email) value ( ?, ?)`
+    const student = await Student.create({
+      name,
+      email,
+    });
 
-  db.execute(insertQuery, [name, email], (err) => {
-    if (err) {
-      console.log(err.message)
-      res.status(500).send(err.message)
-      db.end()
-      return
+    res.status(201).json({
+      message: "Student added successfully",
+      student,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// Read All
+const getStudents = async (req, res) => {
+  try {
+    const students = await Student.findAll();
+
+    res.status(200).json(students);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// Read By ID
+const getStudentById = async (req, res) => {
+  try {
+    const student = await Student.findByPk(req.params.id);
+
+    if (!student) {
+      return res.status(404).json({
+        message: "Student not found",
+      });
     }
-    console.log("student has been inserted")
-    res.status(200).send(`Student with name ${name} has been sucessfully added`)
-  })
-}
 
-const updateStudent = (req, res) => {
-  const { id } = req.params
-  const { name } = req.body
-  const updateQuery = `update students set name = ? where id = ?`
+    res.status(200).json(student);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 
-  db.execute(updateQuery, [name, id], (err, result) => {
-    if (err) {
-      console.log(err.message)
-      res.status(500).send(err.message)
-      db.end()
-      return
-    }
-    if (result.affectedRows === 0) {
-      res.status(404).send("Student not found")
-    }
-    res.status(200).send("User has been updated")
-  })
-}
+// Update
+const updateStudent = async (req, res) => {
+  try {
+    const student = await Student.findByPk(req.params.id);
 
-const deleteStudent = (req, res) => {
-  const { id } = req.params
-  const deleteQuery = `delete from students where id = ?`
-  db.execute(deleteQuery, [id], (err, result) => {
-    if (err) {
-      res.status(500).send("something went wrong")
-      return
+    if (!student) {
+      return res.status(404).json({
+        message: "Student not found",
+      });
     }
-    if (result.affectedRows === 0) {
-      res.status(404).send("Student not found")
-    }
-    res.status(200).send("Student has been deleted")
-  })
-  
-}
 
+    student.name = req.body.name;
+    student.email = req.body.email;
+
+    await student.save();
+
+    res.status(200).json({
+      message: "Student updated successfully",
+      student,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// Delete
+const deleteStudent = async (req, res) => {
+  try {
+    const student = await Student.findByPk(req.params.id);
+
+    if (!student) {
+      return res.status(404).json({
+        message: "Student not found",
+      });
+    }
+
+    await student.destroy();
+
+    res.status(200).json({
+      message: "Student deleted successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 
 module.exports = {
-  studentCheck,
   addStudent,
+  getStudents,
+  getStudentById,
   updateStudent,
-  deleteStudent
-}
+  deleteStudent,
+};
+
+// const db = require("../utils/db")
+
+// const studentCheck = (req, res) => {
+//   res.send("Student is access")
+// }
+
+// const addStudent = (req, res) => {
+//   const { name, email } = req.body
+//   const insertQuery = `insert into students (name, email) value ( ?, ?)`
+
+//   db.execute(insertQuery, [name, email], (err) => {
+//     if (err) {
+//       console.log(err.message)
+//       res.status(500).send(err.message)
+//       db.end()
+//       return
+//     }
+//     console.log("student has been inserted")
+//     res.status(200).send(`Student with name ${name} has been sucessfully added`)
+//   })
+// }
+
+// const updateStudent = (req, res) => {
+//   const { id } = req.params
+//   const { name } = req.body
+//   const updateQuery = `update students set name = ? where id = ?`
+
+//   db.execute(updateQuery, [name, id], (err, result) => {
+//     if (err) {
+//       console.log(err.message)
+//       res.status(500).send(err.message)
+//       db.end()
+//       return
+//     }
+//     if (result.affectedRows === 0) {
+//       res.status(404).send("Student not found")
+//     }
+//     res.status(200).send("User has been updated")
+//   })
+// }
+
+// const deleteStudent = (req, res) => {
+//   const { id } = req.params
+//   const deleteQuery = `delete from students where id = ?`
+//   db.execute(deleteQuery, [id], (err, result) => {
+//     if (err) {
+//       res.status(500).send("something went wrong")
+//       return
+//     }
+//     if (result.affectedRows === 0) {
+//       res.status(404).send("Student not found")
+//     }
+//     res.status(200).send("Student has been deleted")
+//   })
+
+// }
+
+// module.exports = {
+//   studentCheck,
+//   addStudent,
+//   updateStudent,
+//   deleteStudent
+// }
--- routes/student.js
+++ routes/student.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const router = express.Router()
-const studentController = require("../controllers/student")
+const express = require("express");
+const router = express.Router();
 
-router.get("/", studentController.studentCheck)
-router.post("/add", studentController.addStudent)
-router.put("/update/:id", studentController.updateStudent)
-router.delete("/delete/:id",studentController.deleteStudent)
+const studentController = require("../controllers/student");
 
-module.exports = router
+router.post("/", studentController.addStudent);
+router.get("/", studentController.getStudents);
+router.get("/:id", studentController.getStudentById);
+router.put("/:id", studentController.updateStudent);
+router.delete("/:id", studentController.deleteStudent);
+
+module.exports = router;
+
+// const express = require('express')
+// const router = express.Router()
+// const studentController = require("../controllers/student")
+
+// router.get("/", studentController.studentCheck)
+// router.post("/add", studentController.addStudent)
+// router.put("/update/:id", studentController.updateStudent)
+// router.delete("/delete/:id",studentController.deleteStudent)
+
+// module.exports = router
